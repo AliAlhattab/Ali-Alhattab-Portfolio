a895b9c95b8ec61da82ed6487788c58e468eaa83
AliAlhattab <Ali.Alhattab@hotmail.com>
2022-08-04 01:15:45 -0400

    styling on nav

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,18 +1,27 @@
 import React from 'react'
+import './Nav.scss'
+import menu from '../assets/images/Hamburger_icon.svg.png'
+import 'animate.css';
 
 function Nav() {
   return (
     <nav className='nav'>
-        <div className='nav__image'></div>
-        <div className='nav__title'>
-            <p className='nav__name'>Ali Alhattab</p>
-            <p className='nav__subtitle'>Full Stack Web Developer</p>
+        <div className='nav__image animate__animated animate__backInDown'></div>
+        <div className='nav__group'>
+         <div className='nav__title'>
+            <p className='nav__name animate__animated animate__backInLeft'>Ali Alhattab</p>
+            <p className='nav__subtitle animate__animated animate__backInRight'>Full Stack Web Developer</p>
         </div>
         <div className='nav__links'>
-            <img className='nav__link' src='' alt='Home'/>
-            <img className='nav__link' src='' alt='Github'/>
-            <img className='nav__link' src='' alt='LinkedIn'/>
-
+            <img className='nav__menu animate__animated animate__bounceIn animate__delay-1s' src={menu} alt='Menu'/>
+            <ul className='nav__list'>
+                <li className='nav__option'>Home</li>
+                <li className='nav__option'>About Me</li>
+                <li className='nav__option'>Skills</li>
+                <li className='nav__option'>Projects</li>
+                <li className='nav__option'>Contact Me</li>
+            </ul>
+        </div>
         </div>
     </nav>
   )
