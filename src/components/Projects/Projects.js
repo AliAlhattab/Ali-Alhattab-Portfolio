d95406c93969750cad84318630bd965961c6cf8b
AliAlhattab <Ali.Alhattab@hotmail.com>
2022-10-28 00:46:26 -0400

    real commit

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -1,12 +1,13 @@
 import React from 'react'
 import './Projects.scss'
-
+import officequiz from '../../assets/videos/officequiz.mp4'
+import wedevelop from '../../assets/videos/wedevelop.mp4'
 
 function Projects() {
   return (
     <section className='projects'>
         <div className='projects__group1'>
-        <video className='projects__video' controls/>
+        <video className='projects__video' controls src={officequiz}/>
         <p className='projects__text'>The standard chunk of Lorem Ipsum used since the 1500s is reproduced below for those interested. 
             Sections 1.10.32 and 1.10.33 from "de Finibus Bonorum et Malorum" by Cicero are also reproduced in their exact original form, 
             accompanied by English versions from the 1914 translation by H. Rackham.</p>
@@ -15,10 +16,10 @@ function Projects() {
             <p className='projects__text'>The standard chunk of Lorem Ipsum used since the 1500s is reproduced below for those interested. 
             Sections 1.10.32 and 1.10.33 from "de Finibus Bonorum et Malorum" by Cicero are also reproduced in their exact original form, 
             accompanied by English versions from the 1914 translation by H. Rackham.</p>
-            <video className='projects__video' controls />
+            <video className='projects__video' controls src={wedevelop}/>
         </div>
         <div className='projects__group1'>
-        <video className='projects__video' controls />
+        <video className='projects__video' controls src={officequiz}/>
         <p className='projects__text'>The standard chunk of Lorem Ipsum used since the 1500s is reproduced below for those interested. 
             Sections 1.10.32 and 1.10.33 from "de Finibus Bonorum et Malorum" by Cicero are also reproduced in their exact original form, 
             accompanied by English versions from the 1914 translation by H. Rackham.</p>
@@ -27,7 +28,7 @@ function Projects() {
             <p className='projects__text'>The standard chunk of Lorem Ipsum used since the 1500s is reproduced below for those interested. 
             Sections 1.10.32 and 1.10.33 from "de Finibus Bonorum et Malorum" by Cicero are also reproduced in their exact original form, 
             accompanied by English versions from the 1914 translation by H. Rackham.</p>
-            <video className='projects__video' controls />
+            <video className='projects__video' controls src={wedevelop}/>
         </div>
     </section>
   )
